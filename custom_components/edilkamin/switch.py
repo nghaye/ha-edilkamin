28737d0e1fbc589a08390ab66a1fb0d0fc85ebb5
Nicolas Ghaye <nghaye@gmail.com>
2022-12-27 21:14:44 +0100

    chrono switch

--- custom_components/edilkamin/switch.py
+++ custom_components/edilkamin/switch.py
@@ -33,7 +33,8 @@ async def async_setup_entry(
     async_add_entities(
         [
             RelaxSwitch(coordinator, name),
-            StandbySwitch(coordinator, name)
+            StandbySwitch(coordinator, name),
+            ChronoSwitch(coordinator, name)
         ],
         update_before_add=False,
     )
@@ -112,4 +113,44 @@ class StandbySwitch(CoordinatorEntity, SwitchEntity):
         """Turn the entity off."""
         token = self.coordinator.get_token()
         await self.hass.async_add_executor_job(edilkamin.set_standby_mode, token, self._mac_address, False)
+        await self.coordinator.async_refresh()
+
+class ChronoSwitch(CoordinatorEntity, SwitchEntity):
+    """Chrono Mode Entity"""
+
+    def __init__(
+        self,
+        coordinator,
+        name: str,
+    ) -> None:
+        """Create the Edilkamin chrono switch."""
+        super().__init__(coordinator)
+        
+        self._mac_address = coordinator.get_mac()
+
+        self._attr_name = f"{name} Chrono Mode"
+        self._attr_unique_id = f"{self._mac_address}_chronomode"
+        self._attr_icon = "mdi:update"
+        
+        self._attr_device_info = {
+            "identifiers": {("edilkamin", self._mac_address)}
+		}
+
+    @property
+    def is_on(self) -> bool:
+        """Return if standby mode is on."""
+        return self.coordinator.data["status"]["flags"]["is_crono_active"]
+
+    async def turn_on(self, **kwargs) -> None:
+        """Turn the entity on."""
+        token = self.coordinator.get_token()
+        payload = {"name" : "chrono_mode", "value" : True}
+        await self.hass.async_add_executor_job(edilkamin.mqtt_command, token, self._mac_address, payload)
+        await self.coordinator.async_refresh()
+
+    async def turn_off(self, **kwargs) -> None:
+        """Turn the entity off."""
+        token = self.coordinator.get_token()
+        payload = {"name" : "chrono_mode", "value" : False}
+        await self.hass.async_add_executor_job(edilkamin.mqtt_command, token, self._mac_address, payload)
         await self.coordinator.async_refresh()
