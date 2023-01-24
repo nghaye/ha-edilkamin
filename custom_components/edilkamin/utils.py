8c9e18734c57167cf770232b2587eb3ab5c1bb59
Nicolas Ghaye <nghaye@gmail.com>
2023-01-24 13:08:11 +0100

    mac address validation, unique id for discovery, mac address as integration title

---
+++ custom_components/edilkamin/utils.py
@@ -0,0 +1,28 @@
+import re
+
+
+def is_valid_mac_address(str):
+ 
+    # Regex to check valid
+    # MAC address
+    regex = ("^([0-9A-Fa-f]{2}[:-])" +
+             "{5}([0-9A-Fa-f]{2})|" +
+             "([0-9a-fA-F]{4}\\." +
+             "[0-9a-fA-F]{4}\\." +
+             "[0-9a-fA-F]{4})|" +
+             "[0-9a-fA-F]{12}$")
+ 
+    # Compile the ReGex
+    p = re.compile(regex)
+ 
+    # If the string is empty
+    # return false
+    if (str is None):
+        return False
+ 
+    # Return if the string
+    # matched the ReGex
+    if (re.search(p, str)):
+        return True
+    else:
+        return False
